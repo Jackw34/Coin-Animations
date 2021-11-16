b174c74c8453d0fdd2a98cd7370cb84417495e30
Jack <Jwenschhof@bsd220.org>
2021-11-16 10:34:50 -0600

    Initial Commit

---
+++ Coin Animations/Coin_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coin_AnimationsApp.swift
+//  Coin Animations
+//
+//  Created by Student on 11/16/21.
+//
+
+import SwiftUI
+
+@main
+struct Coin_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
